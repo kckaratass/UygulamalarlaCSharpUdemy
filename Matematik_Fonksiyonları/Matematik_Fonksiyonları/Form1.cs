b8803993dfa2004040154eb93d16d910c21c1c27
Kadir Can Karatas <kadircankaratass@gmail.com>
2025-08-07 12:07:24 +0300

    Matematik fonksiyonları işlendi

---
+++ Matematik_Fonksiyonları/Matematik_Fonksiyonları/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Matematik_Fonksiyonları
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sayi = Convert.ToDouble(textBox1.Text);
+            // label2.Text = "Mutlak Değer: " + Math.Abs(sayi).ToString();
+            // label2.Text = "Yuvarlanmış Değer: " + Math.Ceiling(sayi).ToString();
+            // label2.Text = "Yuvarlanmış Değer: " + Math.Floor(sayi).ToString();
+            // label2.Text = "Kuvvet: " + Math.Pow(sayi, 2).ToString();
+            label2.Text = "Karekök: " + Math.Sqrt(sayi).ToString();
+        }
+    }
+}
+
+/*
+ * Abs = Matematiksel mutlak değer fonksiyonudur.
+ * Ceil = Sayıyı bir üst tam sayıya yuvarlar.
+ * Floor = Sayıyı bir alt tam sayıya yuvarlar.
+ * Pow = Sayının kuvvetini alır.
+ * sqrt = Sayının karekökünü alır.
+ */

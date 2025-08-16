f33068b982964b43a28f88482ce65ace42e27a2b
Kadir Can Karatas <kadircankaratass@gmail.com>
2025-08-16 17:03:29 +0300

    Metin belgesi kaydetme konusu işlendi.

---
+++ MetinBelgesi_Kaydetme/MetinBelgesi_Kaydetme/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MetinBelgesi_Kaydetme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(openFileDialog1.FileName); // Seçilen dosyayı satır satır okur.
+                string satir = sr.ReadLine();
+                while (satir != null)
+                {
+                    listBox1.Items.Add(satir);
+                    satir = sr.ReadLine();
+                }
+            }
+        } 
+    }
+}

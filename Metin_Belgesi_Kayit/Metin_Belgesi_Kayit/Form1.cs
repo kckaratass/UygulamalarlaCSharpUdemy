ddcf21406ef278cf0129e32702bbec5f26e6e219
Kadir Can Karatas <kadircankaratass@gmail.com>
2025-08-16 17:31:16 +0300

    Metin belgesine veri kaydetme konusu işlendi.

---
+++ Metin_Belgesi_Kayit/Metin_Belgesi_Kayit/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metin_Belgesi_Kayit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Metin Dosyalar |*.txt";
+            saveFileDialog1.Title = "Metin Belgesi Kaydet";
+            saveFileDialog1.ShowDialog();
+            StreamWriter sw = new StreamWriter(saveFileDialog1.FileName);
+            sw.WriteLine(richTextBox1.Text);
+            sw.Close();
+            MessageBox.Show("Metin Belgesi Kaydedildi");
+        }
+    }
+}

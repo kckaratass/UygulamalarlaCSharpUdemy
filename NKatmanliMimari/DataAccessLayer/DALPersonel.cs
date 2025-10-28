a4081c81feb4ca94b62fde775a71b53dd863baa6
Kadir Can Karatas <kadircankaratass@gmail.com>
2025-10-28 18:49:31 +0300

    DAL Personel listeleme listeleme komutları ve LogicLayer katmanı

---
+++ NKatmanliMimari/DataAccessLayer/DALPersonel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityFramework;
+using EntityLayer;
+using System.Data.SqlClient;    
+namespace DataAccessLayer
+{
+    public class DALPersonel
+    {
+        public static List<EntityPersonel> PersonelListesi()
+        {
+            List<EntityPersonel> degerler = new List<EntityPersonel>();
+            SqlCommand komut1 = new SqlCommand("Select * from TBLBILGI", Baglanti.bgl);
+
+            if (komut1.Connection.State != System.Data.ConnectionState.Open)
+            {
+                komut1.Connection.Open();
+            }
+
+            SqlDataReader dr = komut1.ExecuteReader();
+            while (dr.Read())
+            {
+                EntityPersonel ent = new EntityPersonel();
+                ent.Id = int.Parse(dr["ID"].ToString()); 
+                ent.Ad = dr["AD"].ToString();
+                ent.Soyad = dr["SOYAD"].ToString();
+                ent.Sehir = dr["SEHIR"].ToString();
+                ent.Gorev = dr["GOREV"].ToString();
+                ent.Maas = short.Parse(dr["MAAS"].ToString());
+                degerler.Add(ent);
+            } 
+            dr.Close();
+            return degerler;
+
+        }
+    }
+}
---
+++ NKatmanliMimari/LogicLayer/LogicPersonel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer;
+using DataAccessLayer;
+using EntityFramework;
+
+namespace LogicLayer
+{
+    public class LogicPersonel
+    {
+        public static List<EntityPersonel> LLPersonelListesi()
+        {
+            return DALPersonel.PersonelListesi();
+        }
+    }
+}
